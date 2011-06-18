9e03c03e9176b630b63ba0372f3a01f281c16e03
Owen Nelson <onelson@gmail.com>
2011-06-17 23:48:27 -0400

    working up some tests

--- ipyfield/models.py
+++ ipyfield/models.py
@@ -7,18 +7,20 @@ class IPyField(models.Field):
     """
     
     __metaclass__ = models.SubfieldBase
-    def __init__(self, default=None, *args, **kwargs):
-        super(IPyField, self).__init__(default=default, *args, **kwargs)
+    #def __init__(self, default=None, *args, **kwargs):
+    #    super(IPyField, self).__init__(default=default, *args, **kwargs)
 
     def to_python(self, value):
-        if isinstance(value, IP) or value is None:
+        if not value: 
+            return value
+        elif isinstance(value, IP):
             return value
         else:
             return IP(value)
 
     def get_prep_value(self, value):
         value = self.to_python(value)
-        if value == None:
+        if not value:
             return value
         return value.int()
 
--- ipyfield/tests.py
+++ ipyfield/tests.py
@@ -1,17 +1,30 @@
 from django.test import TestCase
 from django.db.models import Model
 from ipyfield.models import IPyField
+from IPy import IP
+
+
+class DummyModel(Model):
+    field = IPyField()
 
 class IPyFieldTests(TestCase):
 
     def setUp(self):
-        class Obj(Model):
-            '''Some kind of model'''
-            field = IPyField()
+        self.obj = DummyModel()
 
-        self.obj = Obj()
-
-    def test_something(self):
+    def test_iptype(self):
         self.obj.field = '127.0.0.1'
-        self.obj.field.strNormal()
+        self.assertEqual(self.obj.field.iptype(),'PRIVATE')
+        self.obj.field = '100.80.90.100'
+        self.assertEqual(self.obj.field.iptype(), 'PUBLIC')
+
+
+    def test_lookups_in_CIDR(self):
+        ip_block = IP('127.0.0.0/28')
+        [DummyModel.objects.create(field=ip) for ip in ip_block]
+        self.assertEqual(len(ip_block), DummyModel.objects.count())
+        self.assertTrue(DummyModel.objects.count() > 
+               DummyModel.objects.filter(field__in=IP('127.0.0.0/30'))\
+               .count())
+
 
