00e0f36be5f74e07d41d1398beec23fa3b8b8af2
Owen Nelson <onelson@gmail.com>
2011-06-18 14:01:29 -0400

    I suppose other types of lookup should work just fine. __in is the only special case

--- ipyfield/models.py
+++ ipyfield/models.py
@@ -23,15 +23,13 @@ class IPyField(models.Field):
         return value.int()
 
     def get_prep_lookup(self, lookup_type, value):
-        if lookup_type == 'exact':
-            return self.get_prep_value(value)
-        elif lookup_type == 'in':
+        if lookup_type == 'in':
             if isinstance(value, str) and '/' in value:
                 # convert to CIDR iter
                 value = IP(value)
             return [self.get_prep_value(v) for v in value]
-        else:
-            raise TypeError('Lookup type %r not supported.' % lookup_type)
+        else: 
+            return self.get_prep_value(value)
 
     def get_internal_type(self):
         return 'BigIntegerField'
